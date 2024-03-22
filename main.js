d59e210af0b59d4dacc2b0176f306b56400a5e9b
cris24tayler <cristian.tayler@alunoescolassociais.org.br>
2024-03-22 11:30:03 -0300

    adiciona o index, css, js ao projeto

---
+++ main.js
@@ -0,0 +1,10 @@
+function colorirDia(){
+    let days    = document.getElementById('day').value;
+    let color = document.getElementById('color').value;
+    let calendar = document.getElementById('calendar');
+    let td = calendar.getElementsByTagName('td')[(parseInt(days))]
+
+    td.style.backgroudColor = color;
+
+    
+}
