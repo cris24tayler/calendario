8a99d172271eb8364dab8c463519e464bd1cdad9
cris24tayler <cristian.tayler@alunoescolassociais.org.br>
2024-03-28 08:55:12 -0300

    atualiza código

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
+
 function colorirDia(){
     let days    = document.getElementById('day').value;
-    let color = document.getElementById('color').value;
+    let color   = document.getElementById('color').value;
     let calendar = document.getElementById('calendar');
-    let td = calendar.getElementsByTagName('td')[(parseInt(days))]
-
-    td.style.backgroudColor = color;
-
-    
+    let td = calendar.getElementsByTagName('td')[(parseInt(days))]; 
+    td.style.backgroundColor = color;
 }
