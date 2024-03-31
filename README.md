f6ad37f3f31a91e00d38dab88f1cc89ef5c775e3
cris24tayler <cristian.tayler@alunoescolassociais.org.br>
2024-03-30 22:32:55 -0300

    correção de bugs

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# calendario
+# indice
+
+* [Descrição](#descri%C3%A7%C3%A3o)
+
+* [Funcionalidades](#funcionalidades)
+
+* [Fontes utilizadas ](#fontes-utilizadas)
+
+* [Autores](#autores)
+
+## descrição
+Site com objetivo de marcar os dias do mes de abril para marcar eventos por exemplo
+## funcionalidades
+* marcar eventos, festas de aniversário, shows, etc.
+
+## fontes utilizadas
+* inteligencia artificial chat gbt.
+* encinamentos do professor [Leonardo](https://github.com/leonardorochamarista)
+
+### tecnologias utilizadas
+ * git hub
+ * google
+
+## autores
+* Cristian Tayler
--- main.js
+++ main.js
@@ -6,3 +6,18 @@ function colorirDia(){
     let td = calendar.getElementsByTagName('td')[(parseInt(days))]; 
     td.style.backgroundColor = color;
 }
+    // Verifica se o número digitado é válido (não maior que 30)
+    if (parseInt(days) > 30) {
+        alert("Esse número é inválido. O número não pode ser maior do que 30.");
+        return; // Sai da função imediatamente
+    }
+ 
+    // Subtrai 1 do valor do dia porque os arrays em JavaScript começam com índice 0
+    let index = parseInt(days) - 1;
+   
+    // Verifica se o índice está dentro do intervalo válido
+    if (index >= 0 && index < tds.length) {
+        tds[index].style.backgroundColor = color;
+    } else {
+        alert('Dia selecionado está fora do intervalo válido.');
+    }
